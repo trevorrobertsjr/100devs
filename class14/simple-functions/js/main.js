f43c3fdf46123cbc23c7f900cb140c6e5cfa3ceb
Trevor Roberts Jr <Trevor.Roberts.Jr@gmail.com>
2022-06-13 23:30:17 -0400

    created simple functions

---
+++ class14/simple-functions/js/main.js
@@ -0,0 +1,17 @@
+//create a function that adds two numbers and alerts the sum
+function addTwoNumbers(a, b){
+    alert(a+b)
+}
+
+//create a function that multiplys three numbers and console logs the product
+function multiplyThreeNumbers(a,b,c){
+    console.log(a*b*c)
+}
+//create a function that divides two numbers and returns the ???
+function divideTwoNumbers(a,b) {
+    return a/b
+}
+
+addTwoNumbers(3,5)
+multiplyThreeNumbers(1,2,5)
+console.log(divideTwoNumbers(6,3))
